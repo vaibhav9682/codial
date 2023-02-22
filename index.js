0859f56768b3a1ec942ca4096ec247a43a40865d
vaibhav9682 <vaibhav205.vd@gmail.com>
2023-02-22 23:51:50 +0530

    add a controller and router for home

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 9000;
 const app = express();
 
+app.use('/', require('./routes'));
 
 
 
@@ -25,9 +26,8 @@ const app = express();
 
 
 
-
-app.listen(port , function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log(`error in strating express server on port: ${port}`);
     }
     console.log(`express server is running on port: ${port}`);
