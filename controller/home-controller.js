79a73e8e1ec186a0e292eac0e73d2dcf5fc7cd9d
vaibhav9682 <vaibhav205.vd@gmail.com>
2023-03-16 16:25:16 +0530

    showing commets om each post

--- controller/home-controller.js
+++ controller/home-controller.js
@@ -2,19 +2,22 @@ const Post = require('../model/post')
 
 module.exports.home = async function (req, res) {
 
-    let posts = await Post.find({}).populate('user')
- console.log(posts);
+    let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+    console.log(posts);
 
     return res.render('home', {
         title: "Home",
         posts: posts
     })
 
-    // let posts = await Post.find({});
-    // return res.render('home', {
-    //     title: "Home",
-    //     posts: posts
-    // })
+
 
 
 }
